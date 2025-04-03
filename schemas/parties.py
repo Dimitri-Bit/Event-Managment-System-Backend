1cdd58bf9d8d6878f480bc91524e48759ebc4fc3
Dimitri-Bit <dimitrijefustic0@gmail.com>
2025-04-03 17:56:04 +0200

    use str instead of HttpUrl

--- schemas/parties.py
+++ schemas/parties.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional
 
 class PartyCreate(BaseModel):
     name_party: str = Field(min_length=3, max_length=255)
-    url_image_full: HttpUrl
+    url_image_full: str
     name_organizer: str = Field(min_length=3, max_length=255)
     date_start: datetime
     date_end: datetime
@@ -13,13 +13,13 @@ class PartyCreate(BaseModel):
     name_type: str = Field(min_length=3, max_length=255)
     text_entry_fee: Optional[float] = None
     text_more: Optional[str] = Field(max_length=1000)
-    url_organizer: Optional[HttpUrl] = None
-    url_party: Optional[HttpUrl] = None
+    url_organizer: Optional[str] = None
+    url_party: Optional[str] = None
 
 class PartyResponse(BaseModel):
     id: int
     name_party: str
-    url_image_full: HttpUrl
+    url_image_full: str
     name_organizer: str
     date_start: datetime
     date_end: datetime
@@ -28,15 +28,15 @@ class PartyResponse(BaseModel):
     name_type: str
     text_entry_fee: Optional[float] = None
     text_more: Optional[str] = None
-    url_organizer: Optional[HttpUrl] = None
-    url_party: Optional[HttpUrl] = None
+    url_organizer: Optional[str] = None
+    url_party: Optional[str] = None
 
     class Config:
         from_attributes = True
 
 class PartyUpdate(BaseModel):
     name_party: Optional[str] = Field(min_length=3, max_length=255)
-    url_image_full: Optional[HttpUrl]
+    url_image_full: Optional[str]
     name_organizer: Optional[str] = Field(min_length=3, max_length=255)
     date_start: Optional[datetime]
     date_end: Optional[datetime]
@@ -45,8 +45,8 @@ class PartyUpdate(BaseModel):
     name_type: Optional[str] = Field(min_length=3, max_length=255)
     text_entry_fee: Optional[float] = None
     text_more: Optional[str] = Field(max_length=1000)
-    url_organizer: Optional[HttpUrl] = None
-    url_party: Optional[HttpUrl] = None
+    url_organizer: Optional[str] = None
+    url_party: Optional[str] = None
 
     class Config:
         orm_mode = True
