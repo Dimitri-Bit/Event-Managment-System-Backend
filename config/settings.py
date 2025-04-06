a446366746cbb089911742598955f3461ad2086f
Dimitri-Bit <dimitrijefustic0@gmail.com>
2025-04-06 18:19:11 +0200

    alembic get db url from pydantic settings

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DB_URL: str = "sqlite:///db.db"
+    DB_URL_SYNC: str = "sqlite:///db.db"
     SECRET_KEY: str = "test"
     HASH_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
